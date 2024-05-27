b8a14833f9d59309426d0e9c92ac3e3820773545
Elena Yankova <plishivui@gmail.com>
2024-05-27 18:56:21 +0400

    first commit

---
+++ some.php
@@ -0,0 +1,11 @@
+<?php
+$date = [
+        "title" => $_POST['title'],
+    "content" => $_POST['content']
+];
+
+$connect = new PDO('mysql:host=localhost; dbname=ajax-24', 'root', '');
+$sql = 'INSERT INTO `posts` (title, content) VALUES (:title, :content)';
+$statment = $connect->prepare($sql);
+$result = $statment->execute($date);
+var_dump($result);
